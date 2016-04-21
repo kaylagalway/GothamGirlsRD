de5009b949d2b54a51f8ea026f37889e5e443e06
kaylagalway <kayla.galway@gmail.com>
2016-04-20 21:30:55 -0400

    xcode project

---
+++ GGRDApp/GGRDApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GGRDApp
+//
+//  Created by Kayla Galway on 4/20/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GGRDApp/GGRDApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GGRDApp
+//
+//  Created by Kayla Galway on 4/20/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
