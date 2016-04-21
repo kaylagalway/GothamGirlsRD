bfe84e610ea025f7be58280351dc107b7088bf6b
kaylagalway <kayla.galway@gmail.com>
2016-04-20 21:55:21 -0400

    created organizational groups

---
+++ GGRDApp/GGRDApp/NewsTableViewControllerDataSource.h
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewControllerDataSource.h
+//  GGRDApp
+//
+//  Created by Kayla Galway on 4/20/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NewsTableViewControllerDataSource : NSObject
+
+@end
---
+++ GGRDApp/GGRDApp/NewsTableViewControllerDataSource.m
@@ -0,0 +1,13 @@
+//
+//  NewsTableViewControllerDataSource.m
+//  GGRDApp
+//
+//  Created by Kayla Galway on 4/20/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import "NewsTableViewControllerDataSource.h"
+
+@implementation NewsTableViewControllerDataSource
+
+@end
