ed09f3b8b0d6b4c6ec6d0a8076bbed646561aa59
kaylagalway <kayla.galway@gmail.com>
2016-07-09 23:57:35 -0400

    added activity indicator for loading table view. detail view controller layout code currently incorrect/broken

---
+++ GGRDApp/GGRDApp/Views/GGNewsArticleBodyView.h
@@ -0,0 +1,13 @@
+//
+//  GGNewsArticleBodyView.h
+//  GGRDApp
+//
+//  Created by Kayla Galway on 7/9/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GGNewsArticleBodyView : UIView
+
+@end
---
+++ GGRDApp/GGRDApp/Views/GGNewsArticleBodyView.m
@@ -0,0 +1,21 @@
+//
+//  GGNewsArticleBodyView.m
+//  GGRDApp
+//
+//  Created by Kayla Galway on 7/9/16.
+//  Copyright © 2016 edu.self. All rights reserved.
+//
+
+#import "GGNewsArticleBodyView.h"
+
+@implementation GGNewsArticleBodyView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
